a9f05286ae8170473fa00305b0e041f8f0291304
cmatsuta <cmatsuta@jbs.com>
2020-08-29 14:08:58 -0700

    modify html and javascript

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,4 @@
+// variables
 var msgDiv = document.querySelector("#msg");
 var index = 0;
 var correct = 0;
@@ -5,9 +6,11 @@ const start = document.getElementById("start");
 const quiz = document.getElementById("quiz");
 const counter = document.getElementById("counter");
 let quizTime;
-let count = 60;
-const questionTime = 60;
-start.addEventListener("click",startQuiz);
+let count = 40;
+const questionTime = 40;
+start.addEventListener("click", startQuiz);
+
+//variable for score
 var username = document.querySelector("#name");
 var saveBtn = document.querySelector("#save");
 var finalScore = document.querySelector("#finalScore");
@@ -17,67 +20,71 @@ var highScore = JSON.parse(localStorage.getItem("highScore")) || [];
 
 
 // start quiz
-function startQuiz(){
+function startQuiz() {
     start.style.display = "none";
     renderQuestion();
     quiz.style.display = "block";
     renderCounter();
-    quizTime = setInterval(renderCounter,1000); // 1000ms = 1s
+    quizTime = setInterval(renderCounter, 1000); // 1000ms = 1s
 }
 
-function init(x){
+function init(x) {
     return document.getElementById(x);
 }
 
-function renderQuestion(){
+// render question function
+function renderQuestion() {
     test = init("test");
-    if(index >= questions.length){
-        test.innerHTML = "<h2>Your score is "+correct+" of "+questions.length+" .</h2>";
+    if (index >= questions.length) {
+        test.innerHTML = "<h2>Your score is " + correct + " of " + questions.length + " .</h2>";
         init("renderQuestions").innerHTML = "All Done!";
         index = 0;
         correct = 0;
         return false;
     }
 
-    init("renderQuestions").innerHTML = "Question "+(index+1)+" of "+questions.length;
+    init("renderQuestions").innerHTML = "Question " + (index + 1) + " of " + questions.length;
     question = questions[index][0];
     choiceA = questions[index][1];
     choiceB = questions[index][2];
     choiceC = questions[index][3];
     choiceD = questions[index][4];
-    test.innerHTML = "<h3>"+question+"</h3>";
-    test.innerHTML += "<input type='radio' name='choices' value='A'> "+choiceA+"<br>";
-    test.innerHTML += "<input type='radio' name='choices' value='B'> "+choiceB+"<br>";
-    test.innerHTML += "<input type='radio' name='choices' value='C'> "+choiceC+"<br>";
-    test.innerHTML += "<input type='radio' name='choices' value='D'> "+choiceD+"<br>";
+    test.innerHTML = "<h3>" + question + "</h3>";
+    test.innerHTML += "<input type='radio' name='choices' value='A'> " + choiceA + "<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='B'> " + choiceB + "<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='C'> " + choiceC + "<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='D'> " + choiceD + "<br>";
     test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
 }
 
-function renderCounter(){
-        counter.innerHTML = count + " sec left";
-        count--;
-        if(count <= -1){
+// rendr counter function
+function renderCounter() {
+    counter.innerHTML = count + " sec left";
+    count--;
+    if (count <= -1) {
         clearInterval(quizTime);
         // count = 0;
-        }
+    }
 }
 
+// calling the function to display message when answer
 function displayMessage(type, message) {
     msgDiv.textContent = message;
     msgDiv.setAttribute("class", type);
-  }
+}
 
-function checkAnswer(){
+// check answer from the multiple choice questions
+function checkAnswer() {
     choices = document.getElementsByName("choices");
-    for(var i=0; i<choices.length; i++){
-        if(choices[i].checked){
+    for (var i = 0; i < choices.length; i++) {
+        if (choices[i].checked) {
             choice = choices[i].value;
         }
     }
-    if(choice == questions[index][5]){
+    if (choice == questions[index][5]) {
         correct++;
         displayMessage("correct", "Correct!");
-    }else{
+    } else {
         displayMessage("wrong", "Wrong Answer!");
         count -= 5;
     }
